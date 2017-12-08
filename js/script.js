1fc170262163812c1cbb7a609717cdf179c52104
Mostafa Elsheikh <abdelaleem@gmx.com>
2017-12-08 20:52:22 +0200

    second requirement

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-var $click = $('#clicks');
-var clicks = $click.text();
-$('#Cat').click(function () {
-    $click.text(++clicks)
+var $click1 = $('#clicks1');
+var clicks1 = $click1.text();
+$('#Cat1').click(function () {
+    $click1.text(++clicks1)
+});
+var $click2 = $('#clicks2');
+var clicks2 = $click2.text();
+$('#Cat2').click(function () {
+    $click2.text(++clicks2)
 });
