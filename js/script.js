6419b54fd809b48ed4febaabe37f849e29c35317
Mostafa Elsheikh <abdelaleem@gmx.com>
2017-12-11 19:22:28 +0200

    cat clicker premium pro

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-$(document).ready(function () {
    // var $img = $('img');
 // var $b = $('b');
 // var clicks = [];
@@ -13,15 +12,17 @@ $(document).ready(function () {
 
 var model = {
     init: function () {
-        this.cats = ['images/625069434_bf6e0d6ea4_o.jpg',
+        this.catsImg = ['images/625069434_bf6e0d6ea4_o.jpg',
             'images/2290467335_d4fd0b3bd7_o.jpg',
             'images/4638426067_90c4df41e9_o.jpg',
             'images/5616147572_bf5ec3201f_o.jpg',
             'images/13772619545_71e66591a8_o.jpg',
             'images/unnamed.jpg'];
         this.clicks = [];
-        for(var i = 0; i < this.cats.length; i++){
+        this.name = [];
+        for(var i = 0; i < this.catsImg.length; i++){
             this.clicks.push(0);
+            this.name.push('Cat'+(i+1));
         }
         this.currentCat = 0;
     },
@@ -29,7 +30,7 @@ var model = {
         return this.currentCat;
     },
    getCat: function (i) {
-       return this.cats[i];
+       return this.catsImg[i];
    },
     incrClick: function () {
       ++this.clicks[this.getCurrentCat()];
@@ -38,7 +39,7 @@ var model = {
        return this.clicks[i];
    },
    getCats: function () {
-       return this.cats;
+       return this.catsImg;
    },
    getClicks: function () {
        return this.clicks;
@@ -83,16 +84,41 @@ var octupus = {
         view.clicks.innerText = model.getClick(model.getCurrentCat());
     },
     renderCurrentClick: function () {
+        this.renderAdminData();
         view.clicks.innerText = model.getCurrentClick();
+    },
+    renderAdminData: function () {
+        var name = model.name[this.getCurrentCat()];
+        var url = model.catsImg[this.getCurrentCat()];
+        var clicks = model.clicks[this.getCurrentCat()];
+        view.labelName.value = name;
+        view.labelURL.value = url;
+        view.labelClicks.value = clicks;
+    },
+    toggleAdminView: function () {
+        this.renderAdminData();
+        view.toggleAdminView();
+    },
+    updateAdminData: function () {
+        model.name[this.getCurrentCat()] = view.labelName.value;
+        model.catsImg[this.getCurrentCat()] = view.labelURL.value;
+        view.cat.src = model.catsImg[this.getCurrentCat()];
+        model.clicks[this.getCurrentCat()] = view.labelClicks.value;
+        view.clicks.innerText = model.clicks[this.getCurrentCat()];
     }
 };
 
 var view = {
    init: function () {
-       var cat = document.getElementById('cat');
+       this.cat = document.getElementById('cat');
        this.clicks = document.getElementById('clicks');
        this.navBar = document.getElementsByClassName('navbar-nav');
        this.navLinks = [];
+       this.adminView = document.getElementById('adminView');
+       this.toggleAdminView();
+       this.labelName = document.getElementById('catName');
+       this.labelURL = document.getElementById('imgURL');
+       this.labelClicks = document.getElementById('noClicks');
        for(var i = 0; i < octupus.getNumClicks().length; i++){
            var navItem = document.createElement('li');
            navItem.classList.add('nav-item');
@@ -108,11 +134,11 @@ var view = {
                   cat.src = newSrcCopy;
                   octupus.setCurrentCat(iCopy);
                   octupus.renderCurrentClick();
+                  octupus.renderAdminData();
                };
            }(newSrc, i));
        }
        cat.addEventListener('click', function () {
-           console.log(this.clicks);
            octupus.incrClick();
        });
    },
@@ -121,10 +147,19 @@ var view = {
    },
    switchCats: function (i, src) {
        this.cat.setAttribute('src', src);
-   }
+   },
+    updateInfo: function () {
+        
+    },
+    toggleAdminView: function () {
+       if(this.adminView.style.visibility === 'hidden'){
+            this.adminView.style.visibility = 'visible';
+       } else {
+            this.adminView.style.visibility = 'hidden';
+       }
+    }
 
 };
 
 octupus.init();
 
-});
